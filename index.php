8f0aff9b6db25ac2978505fb633e788b8e91ce4a
EmmanuelOluwafemi <46839679+EmmanuelOluwafemi@users.noreply.github.com>
2020-07-30 10:59:07 +0100

    Responsiveness fix

--- index.php
+++ index.php
@@ -122,13 +122,13 @@
 
     </section>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
-    <script src="./assets/js/main.js"></script>
     <script>
         // Loader
         setTimeout(function(){
             $(".loader-bg").fadeToggle();
         }, 1500);
     </script>
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <script src="./assets/js/main.js"></script>
 </body>
 </html>
